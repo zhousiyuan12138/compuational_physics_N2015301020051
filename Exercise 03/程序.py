8a395f31175e7e33bd3d356876032c0722f403f5
zhousiyuan12138 <31839912+zhousiyuan12138@users.noreply.github.com>
2017-09-29 12:44:16 +0800

    Create 程序.py

---
+++ Exercise 03/程序.py
@@ -0,0 +1,33 @@
+```python
+import numpy as np 
+
+import matplotlib.pyplot as plt
+
+x = [0.]  
+t = [0.] 
+v = 40 
+dt = 0.1
+end_time = 20
+
+for i in range(int(end_time / dt)):
+
+	tmp = x[i] + v * dt
+
+	x.append(tmp)
+
+	t.append(dt * (i + 1))     #euler 法
+
+	print t[-1], x[-1]
+
+plt.figure(figsize=(8,6)) 
+
+plt.plot(t,x,label="x(t)",color="red",linewidth=1)
+
+plt.xlabel("t(s)") 
+
+plt.ylabel("x(m)") 
+
+plt.legend()  
+
+plt.show() 
+```
